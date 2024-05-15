0434e1ba88fca8c1ddfde14454109f40eb857091
mahmud444t <ferdausemahmud@gmail.com>
2024-05-15 23:16:38 +0600

    Marking page responsive

--- src/PrivateComponents/MarkingPage.jsx
+++ src/PrivateComponents/MarkingPage.jsx
@@ -57,35 +57,41 @@ const MarkingPage = () => {
                     <div className="loading loading-spinner loading-lg"></div>
                 </div>
             ) : <>
-                <div className="flex justify-center items-center my-12">
-                    <div className="">
-                        {assignmentData ? (
-                            <>
-                                <div className="card bg-base-100 shadow-xl border">
-                                    <form action="" onSubmit={handleSubmit}>
-                                        <div className="card-body">
-                                            <div className="flex items-center justify-center text-3xl font-semibold"><h2>{assignmentData.title}</h2></div>
-                                            <p><span className="text-xl font-semibold">Note: </span>{assignmentData.note}</p>
-                                            <p> <span className="text-xl font-semibold">PDF/Docs Link:</span> <a target="_blank" className="hover:underline hover:text-blue-400" href={assignmentData.file}>{assignmentData.file}</a> </p>
-                                            <br />
-                                            <div className="flex flex-col">
-                                                <label htmlFor="marks" className="text-xl font-medium">Marks:</label>
-                                                <input type="number" onChange={(e) => setObtainedMark(e.target.value)} required id="marks" placeholder="Enter marks" className="border border-gray-400  rounded-md p-4 mx-1" />
-                                            </div>
-                                            <div className="flex flex-col">
-                                                <label htmlFor="feedback" className="text-xl font-medium">Feedback:</label>
-                                                <textarea name="feedback" onChange={(e) => setFeedback(e.target.value)} rows={5} required placeholder="Enter feedback" className="border border-gray-400 resize-none rounded-md p-4 mx-1"></textarea>
-                                            </div>
-                                            <br />
-                                            <button type="submit" className="btn btn-primary mt-2">Submit</button>
+                <div className="md:w-1/2 mx-auto text-center my-12 px-4">
+                    {assignmentData ? (
+                        <>
+                            <div className="card bg-base-100 shadow-xl border">
+                                <form action="" onSubmit={handleSubmit}>
+
+                                    <div className="flex items-center justify-center text-3xl font-semibold"><h2>{assignmentData.title}</h2></div>
+                                    <div className="card-body !text-left">
+                                        <div>
+                                            <p><span className="text-xl font-semibold">Note: </span></p><br />
+                                            <p className="break-words">{assignmentData.note}</p>
                                         </div>
-                                    </form>
-                                </div>
-                            </>
-                        ) : (
-                            <p></p>
-                        )}
-                    </div>
+                                        <div>
+                                            <p className="text-xl font-semibold">PDF/Docs Link:</p>
+                                            <br />
+                                            <a target="_blank" className="hover:underline break-all hover:text-blue-400" href={assignmentData.file}>{assignmentData.file}</a>
+                                        </div>
+                                        <br />
+                                        <div className="flex flex-col">
+                                            <label htmlFor="marks" className="text-xl font-medium">Marks:</label>
+                                            <input type="number" onChange={(e) => setObtainedMark(e.target.value)} required id="marks" placeholder="Enter marks" className="border border-gray-400  rounded-md p-4 mx-1" />
+                                        </div>
+                                        <div className="flex flex-col">
+                                            <label htmlFor="feedback" className="text-xl font-medium">Feedback:</label>
+                                            <textarea name="feedback" onChange={(e) => setFeedback(e.target.value)} rows={5} required placeholder="Enter feedback" className="border border-gray-400 resize-none rounded-md p-4 mx-1"></textarea>
+                                        </div>
+                                        <br />
+                                        <button type="submit" className="btn btn-primary mt-2">Submit</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </>
+                    ) : (
+                        ''
+                    )}
                 </div>
             </>}
         </div>
